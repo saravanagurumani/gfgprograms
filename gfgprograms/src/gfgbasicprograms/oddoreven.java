5e200a5b2e15778228b1db6b910645ebe023ac61
saravanagurumani <itssaravana2001@gmail.com>
2024-06-07 15:39:06 +0530

    written odd oe even program

--- gfgprograms/src/gfgbasicprograms/oddoreven.java
+++ gfgprograms/src/gfgbasicprograms/oddoreven.java
@@ -1,10 +1,29 @@
 package gfgbasicprograms;
 
+import java.util.Scanner;
+
 public class oddoreven {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan =new Scanner (System.in);
+		System.out.println("Enter number :");
+		int userin=scan.nextInt();
+		oddeven(userin);		
 
+	}
+
+	public static void oddeven(int check) {
+		 if(check==0) {
+			System.out.println("Invalid");
+		}
+		else if(check % 2 == 0) {
+			System.out.println("EVEN NUMBER");
+		}
+		else {
+			System.out.println("ODD NUMBER");
+		}
+		
 	}
 
 }
