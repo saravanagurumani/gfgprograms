b40ffde64bba04ae79b754f6a9f7985af44e6fa5
saravanagurumani <itssaravana2001@gmail>
2024-07-09 20:31:15 +0530

    add file

---
+++ gfgprograms/src/gfgbasicprograms/Gcdoftwonumber.java
@@ -0,0 +1,25 @@
+package gfgbasicprograms;
+
+import java.util.Scanner;
+
+public class Gcdoftwonumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int gcd=0;
+		Scanner scan = new Scanner(System.in); 
+		System.out.println("Enter first number :");
+		int a =scan.nextInt();
+		System.out.println("Enter second number :");
+		int b =scan.nextInt();
+		for(int i=1;i<=a&&i<=b;i++) {
+			if(a%i==0&&b%i==0) {     // check greatest among common divide number
+			System.out.println("Commonly divided number "+ i);
+				gcd=i;              //declare the  greatest among common divide number to gcd variable
+			}
+		}
+		System.out.println("%dAmong the commonly divided number in greatest number is "+ gcd);
+		
+	}
+	
+}
